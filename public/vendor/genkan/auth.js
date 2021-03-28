b617699dba3a0ea81acf799b5778f4e609b29bc5
Dylan Kok <tenkotofu@gmail.com>
2021-03-28 16:18:02 +0800

    Fixed issue with "hold to show password"

--- public/vendor/genkan/auth.js
+++ public/vendor/genkan/auth.js
@@ -1,13 +1,13 @@
 // Show password on mousedown on Sign up form
 $('#showPassword').on('mousedown taphold', function () {
-    $("#registerPassword").attr("type", "text")
-    $("#registerPassword").attr("class", "input is-primary is-active")
+    $("#newPassword").attr("type", "text")
+    $("#newPassword").attr("class", "input is-primary is-active")
     $("#icon-showPassword").addClass("fa-eye")
     $("#icon-showPassword").removeClass("fa-eye-slash")
     $("#showPasswordText").text("Release to hide password")
 }).on('mouseup mouseleave taphold', function () {
-    $("#registerPassword").attr("type", "password")
-    $("#registerPassword").attr("class", "input")
+    $("#newPassword").attr("type", "password")
+    $("#newPassword").attr("class", "input")
     $("#icon-showPassword").addClass("fa-eye-slash")
     $("#icon-showPassword").removeClass("fa-eye")
     $("#showPasswordText").text("Hold to show password")
